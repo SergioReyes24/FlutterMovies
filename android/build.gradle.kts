64e083e252d68750ab67cb7bbae2d93000074d60
Sergio Reyes <rafitar370@gmail.com>
2025-11-17 16:13:19 -0600

    Actividad 3.7. Integración de base de datos 

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,22 +1,17 @@
+plugins {
+    // Usa las versiones que Gradle ya tiene cargadas
+    id("com.android.application") version "8.9.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+    // Flutter plugin: no fijes versión manualmente
+    id("dev.flutter.flutter-gradle-plugin") apply false
+    id("com.google.gms.google-services") version "4.3.15" apply false
+}
+
 allprojects {
     repositories {
         google()
         mavenCentral()
     }
-}
-
-val newBuildDir: Directory =
-    rootProject.layout.buildDirectory
-        .dir("../../build")
-        .get()
-rootProject.layout.buildDirectory.value(newBuildDir)
-
-subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-subprojects {
-    project.evaluationDependsOn(":app")
 }
 
 tasks.register<Delete>("clean") {
